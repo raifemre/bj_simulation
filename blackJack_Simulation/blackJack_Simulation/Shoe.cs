d95cb3518dbb540adbcc0ff0400944fc80cf5e28
Denizzi <yamandnz@gmail.com>
2016-02-02 23:01:26 +0200

    Shoe Class
    First commit

---
+++ blackJack_Simulation/blackJack_Simulation/Shoe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJackSimulation
+{
+    class Shoe
+    {
+        private int _deckAmount;
+        private int _totalCards;
+        public int[] Cards;
+
+        public Shoe(int deckAmount)
+        {
+            _deckAmount = deckAmount;
+            _totalCards = _deckAmount * 52;
+            Cards = new int[11];
+            GenerateDeck();
+        }
+
+        public void GenerateDeck()
+        {
+            Cards[0] = _totalCards;
+            Cards[10] = 4 * 4 * _deckAmount;
+
+            for (int i = 1; i < 10; i++)
+                Cards[i] = 4 * _deckAmount;
+        }
+    }
+}
