57fc3f34ab5fdb6e6a11a4e8bd917a370d159789
Denizzi <yamandnz@gmail.com>
2016-02-03 15:48:33 +0200

    Player, Hand Classes

---
+++ blackJack_Simulation/blackJack_Simulation/Hand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackjackSimulation
+{
+    class Hand
+    {
+        private List<int> CardsInHand;
+
+        public Hand()
+        {
+            CardsInHand = new List<int>();
+        }
+        
+        public void AddCard(int cardValue)
+        {
+            CardsInHand.Add(cardValue);
+        }
+
+        //[0] = total, [1] = softTotal
+        public int[] GetValues()
+        {
+            int total = 0, softTotal = 0;
+            bool HasAce = false;
+
+            for (int i = 0; i < CardsInHand.Count; ++i)
+            {
+                total += CardsInHand[i];
+                if (CardsInHand[i] == 1)
+                    HasAce = true;
+            }
+            if (HasAce && total < 12)
+            {
+                softTotal = total;
+                softTotal += 10;
+                if (softTotal == 21)
+                    return new int[] { softTotal };
+                return new int[] { total, softTotal };
+            }
+            return new int[] { total };
+        }
+    }
+}
---
+++ blackJack_Simulation/blackJack_Simulation/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackjackSimulation
+{
+    class Player
+    {
+        public bool IsDealer { get; set; }
+        public Hand _Hand = new Hand();
+
+        public Player(bool isDealer)
+        {
+            IsDealer = isDealer;
+        }
+    }
+}
